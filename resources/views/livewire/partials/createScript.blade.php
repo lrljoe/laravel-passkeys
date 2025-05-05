9a03da5ec2c4b6ff3d684a57a0c9d4863f911094
Rias <hey@rias.be>
2025-05-05 14:34:58 +0200

    Use Livewire @script

--- resources/views/livewire/partials/createScript.blade.php
+++ resources/views/livewire/partials/createScript.blade.php
@@ -1,11 +1,11 @@
+@script
 <script>
-    document.addEventListener('DOMContentLoaded', function () {
-        Livewire.on('passkeyPropertiesValidated', async function (eventData) {
-            const passkeyOptions = eventData[0].passkeyOptions;
+    Livewire.on('passkeyPropertiesValidated', async function (eventData) {
+        const passkeyOptions = eventData[0].passkeyOptions;
 
-            const passkey = await startRegistration(passkeyOptions);
+        const passkey = await startRegistration(passkeyOptions);
 
-            @this.call('storePasskey', JSON.stringify(passkey));
-        });
+        @this.call('storePasskey', JSON.stringify(passkey));
     });
 </script>
+@endscript
